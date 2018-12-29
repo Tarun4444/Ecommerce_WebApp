71e80c7179149f5b94328da16da596eec684558e
Tarun_Inginim <Tarun4444>
2018-12-29 22:35:29 +0530

    Designed the structure and features @ FrontEnd

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,12 +7,26 @@ import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 import { HttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app.router';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RecommendationsComponent } from './recommendations/recommendations.component';
+import { FooterComponent } from './footer/footer.component';
+import { ShopByCategoryComponent } from './shop-by-category/shop-by-category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShowDataComponent,
-    PagenotfoundComponent
+    PagenotfoundComponent,
+    HeaderComponent,
+    ShoppingCartComponent,
+    WishlistComponent,
+    ProfileComponent,
+    RecommendationsComponent,
+    FooterComponent,
+    ShopByCategoryComponent
   ],
   imports: [
     HttpClient,
